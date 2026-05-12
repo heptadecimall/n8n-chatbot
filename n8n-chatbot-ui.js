37c2513477a4e10b941afe9c7eb2a9b9a16ccff3
heptadecimall <106981387+heptadecimall@users.noreply.github.com>
2026-05-12 15:42:12 +0800

    update  chatbot bg

--- n8n-chatbot-ui.js
+++ n8n-chatbot-ui.js
@@ -87,6 +87,12 @@
             display: block;
             margin-bottom: 2px;
         }
+
+        .bg{
+            backgroundSize: "40px 40px",
+            WebkitMaskImage:"radial-gradient(ellipse 70% 60% at 50% 0%, #000 60%, transparent 100%)",
+            maskImage:"radial-gradient(ellipse 70% 60% at 50% 0%, #000 60%, transparent 100%)"
+        }
         `;
     document.head.appendChild(styleSheet);
 
@@ -96,7 +102,7 @@
     wrapper.innerHTML = `
         <button class="naten-chat-toggle">
             <svg  width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 13.5997 2.37562 15.1116 3.04346 16.4525C3.22094 16.8088 3.28001 17.2161 3.17712 17.6006L2.58151 19.8267C2.32295 20.793 3.20701 21.677 4.17335 21.4185L6.39939 20.8229C6.78393 20.72 7.19121 20.7791 7.54753 20.9565C8.88837 21.6244 10.4003 22 12 22Z" fill="#ffffff"></path> </g></svg></button>
-        <div class="naten-chat-box">
+        <div class="naten-chat-box bg">
             <div style="padding:20px; border-bottom:1px solid #eee; display:flex; align-items:center; gap:10px;">
                 <img src="${config.branding.logo}" style="width:30px; height:30px;">
                 <div>
